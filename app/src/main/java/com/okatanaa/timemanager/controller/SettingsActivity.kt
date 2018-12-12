d85e9e5fded807de93f8373dcec32a4aa756c458
oKatanaaa <whitemarsstudios@gmail.com>
2018-12-12 23:18:27 +0400

    Setting global start time also uses TimePickerFragment

--- app/src/main/java/com/okatanaa/timemanager/controller/SettingsActivity.kt
+++ app/src/main/java/com/okatanaa/timemanager/controller/SettingsActivity.kt
@@ -1,18 +1,23 @@
 package com.okatanaa.timemanager.controller
 
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TimePicker
 import android.widget.Toast
 import com.okatanaa.timemanager.R
 import com.okatanaa.timemanager.additional_classes.TextClickedListener
+import com.okatanaa.timemanager.controller.fragments.TimePickerFragment
 import com.okatanaa.timemanager.model.Time
 import com.okatanaa.timemanager.services.Settings
+import com.okatanaa.timemanager.utilities.BUNDLE_TIME
 import com.okatanaa.timemanager.utilities.EXTRA_EDITED_VALUE
 import kotlinx.android.synthetic.main.activity_settings.*
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,21 +26,17 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     fun onStartingTimeClicked(view: View) {
-        TextClickedListener.onClick(this, STARTING_TIME, globalStartTimeTxt.text.toString())
+        val globalTime = Time(this.globalStartTimeTxt.text.toString())
+        val timeBundle = Bundle()
+        timeBundle.putInt(BUNDLE_TIME, globalTime.minutes)
+        val timeFragment = TimePickerFragment()
+        timeFragment.arguments = timeBundle
+        timeFragment.show(supportFragmentManager, "Global start time")
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        try {
-            val newTime = Time(data?.getStringExtra(EXTRA_EDITED_VALUE)!!)
-            Settings.globalStartTime = newTime
-            globalStartTimeTxt.text = newTime.toString()
-        } catch (e: IllegalArgumentException) {
-            Toast.makeText(this, "Wrong time value!", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    companion object {
-        val STARTING_TIME = "Starting time"
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        val newTime = Time(hourOfDay, minute)
+        Settings.globalStartTime = newTime
+        globalStartTimeTxt.text = newTime.toString()
     }
 }
